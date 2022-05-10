71dc5e33f921b375f478196ea783e044cc4eb982
TyperGuy <ray.developper@gmail.com>
2022-05-10 16:11:04 +0100

    feat : create widgetform component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,7 +3,7 @@ import { Popover } from '@headlessui/react'
 import { WidgetForm } from './WidgetForm';
 export function Widget(){
   return(
-    <Popover className="absolute bottom-5 right-5 flex flex-col items-end">
+    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
       <Popover.Panel> 
         <WidgetForm/>
       </Popover.Panel>
