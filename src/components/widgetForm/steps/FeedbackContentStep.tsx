cb3e91f6c46f22888813db9874882b8dc24414e5
TyperGuy <ray.developper@gmail.com>
2022-05-11 14:12:06 +0100

    feat : add report form

--- src/components/widgetForm/steps/FeedbackContentStep.tsx
+++ src/components/widgetForm/steps/FeedbackContentStep.tsx
@@ -20,9 +20,12 @@ export function FeedbackContentStep({feedbackType,onRestart}: FeedbackContentSte
           </span>
          <CloseButton/>
       </header>
-      <div className="flex py-8 gap-2 w-full">
-        
-      </div>
+      <form className="my-4 w-full">
+        <textarea 
+          className="min-w-[304px] min-h-[112px] w-full text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md border-2 focus:border-brand-500 focus:outline-none focus:ring-brand-500 focus:ring-1 resize-none "
+          placeholder="Conte com detalhe o que está acontecendo..."
+        />
+      </form>
     </>
   
   )
