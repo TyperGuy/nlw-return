b634513448796b81acc44b5ca86974f7dcaff716
TyperGuy <ray.developper@gmail.com>
2022-05-11 15:18:36 +0100

    feat : add take-picture button

--- src/components/widgetForm/steps/FeedbackContentStep.tsx
+++ src/components/widgetForm/steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from 'phosphor-react';
+import { ArrowLeft, Camera } from 'phosphor-react';
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
 
@@ -26,6 +26,13 @@ export function FeedbackContentStep({feedbackType,onRestart}: FeedbackContentSte
           placeholder="Conte com detalhe o que está acontecendo..."
         />
         <footer className="flex gap-2 mt-2">
+          <button
+          type="button"
+          className="p-2 bg-zinc-800 rounded-md border-transparent   text-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            <Camera className="w-6 h-6"/>
+          </button>
+
           <button
           type="submit"
           className="p-2 bg-brand-500 rounded-md border-transparent  flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
